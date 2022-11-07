93e76840599b0ebd275491815f1c8e3b3dbdeb9e
HrvojeCukusicOSS <hc46855@oss.unist.hr>
2022-11-07 12:50:36 +0100

    Added initial project files

---
+++ wingedhussar/src/main/java/com/itsaboulder/wingedhussar/WingedhussarApplication.java
@@ -0,0 +1,13 @@
+package com.itsaboulder.wingedhussar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingedhussarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WingedhussarApplication.class, args);
+	}
+
+}
---
+++ wingedhussar/src/test/java/com/itsaboulder/wingedhussar/WingedhussarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsaboulder.wingedhussar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WingedhussarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
